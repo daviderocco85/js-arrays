9623d0eb836ed031395c7cb6357739d8fdc63697
daviderocco85 <roccodavide85@gmail.com>
2026-05-16 01:07:47 +0200

    feat: add solutions for 02-arrays-bonus exercises

---
+++ 02-arrays-bonus/script.js
@@ -0,0 +1,76 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Inverti l'ordine degli insegnanti nell'array teachers
+// e salva il risultato nella variabile reversedTeachers
+const reversedTeachers = [];
+
+for (let i = teachers.length - 1; i >= 0; i--) {
+     
+     reversedTeachers.push(teachers[i]);
+}
+
+console.log('es.1:',reversedTeachers);
+
+
+// 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
+// con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = [];
+
+for (let i=0; i < teachers.length; i++){
+
+  const nameTeachersSize = teachers[i];
+
+  if(nameTeachersSize.length >=5){
+  
+    longNames.push(nameTeachersSize); 
+ 
+  }
+ 
+}
+
+console.log('es.2:',longNames);
+
+
+
+// 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(1,1);
+console.log('es.3:',teachers);
+
+// 4. Verifica se 'Fabio' è presente nell'array teachers
+// e salva il risultato nella variabile isFabioPresent
+let isFabioPresent = false;
+
+for (let i=0; i<teachers.length; i++){
+  
+  if (teachers[i] === 'Fabio'){
+    
+    isFabioPresent = true;
+    break;
+  }
+}
+
+console.log('es.4:',isFabioPresent);
+
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  
+  teachersString += teachers[i];
+
+  
+  if (i < teachers.length - 1) {
+    teachersString += ', ' ;
+  }
+}
+console.log('es.5:',teachersString);
